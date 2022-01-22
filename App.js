135f17155fdeb4e8285b5c3982e5b97d945dfaeb
nordeeen <ahmadnrdnsah@gmail.com>
2022-01-22 10:51:10 +0000

    merevisi styling dan menambahkan gopay feature section

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ const App = () => {
   return (
     <View style={styles.container}>
       <View style={styles.nav}>
+        {/* Search Bar*/}
         <View style={styles.wrapperBar}>
           <View style={styles.wrapperInputSearch}>
             <TextInput
@@ -23,37 +24,77 @@ const App = () => {
             />
           </View>
         </View>
+        {/* Gopay */}
+        <View style={styles.wrapperFirstSectionGopay}>
+          <View style={styles.wrapperSectionGopay}>
+            <Image
+              source={require('./icons/gopay.png')}
+              style={styles.iconGopay}
+            />
+            <Text style={styles.textGopay}>Rp 50.0000</Text>
+          </View>
+          <View style={styles.wrapperSecondSectionGopay}>
+            <View style={styles.iconSecGopayOne}>
+              <Image
+                source={require('./icons/pay.png')}
+                style={styles.iconPictureGopay}
+              />
+              <Text style={styles.textSecGopay}>Pay</Text>
+            </View>
+            <View style={styles.iconSecGopayTwo}>
+              <Image
+                source={require('./icons/nearby.png')}
+                style={styles.iconPictureGopay}
+              />
+              <Text style={styles.textSecGopay}>Nearby</Text>
+            </View>
+            <View style={styles.iconSecGopayThree}>
+              <Image
+                source={require('./icons/top-up.png')}
+                style={styles.iconPictureGopay}
+              />
+              <Text style={styles.textSecGopay}>Top Up</Text>
+            </View>
+            <View style={styles.iconSecGopayFour}>
+              <Image
+                source={require('./icons/more.png')}
+                style={styles.iconPictureGopay}
+              />
+              <Text style={styles.textSecGopay}>More</Text>
+            </View>
+          </View>
+        </View>
       </View>
       <View style={styles.bottomNav}>
-        <View style={styles.colorOne}>
+        <View style={styles.iconOne}>
           <Image
             source={require('./icons/home-active.png')}
             style={styles.icons}
           />
           <Text style={styles.font}>Home</Text>
         </View>
-        <View style={styles.colorTwo}>
+        <View style={styles.iconTwo}>
           <Image
             source={require('./icons/order-active.png')}
             style={styles.icons}
           />
           <Text style={styles.font}>Orders</Text>
         </View>
-        <View style={styles.colorThree}>
+        <View style={styles.iconThree}>
           <Image
             source={require('./icons/help-active.png')}
             style={styles.icons}
           />
           <Text style={styles.font}>Help</Text>
         </View>
-        <View style={styles.colorFour}>
+        <View style={styles.iconFour}>
           <Image
             source={require('./icons/inbox-active.png')}
             style={styles.icons}
           />
           <Text style={styles.font}>Inbox</Text>
         </View>
-        <View style={styles.colorFive}>
+        <View style={styles.iconFive}>
           <Image
             source={require('./icons/account-active.png')}
             style={styles.icons}
@@ -71,34 +112,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  text: {
-    fontSize: 20,
-    color: 'white',
-    textAlign: 'center',
-    marginTop: '50%',
-  },
-  bottomNav: {
-    height: 54,
-    flexDirection: 'row',
-  },
   nav: {
     flex: 1,
     backgroundColor: 'white',
   },
-  icons: {
-    height: 26,
-    width: 26,
-  },
-  font: {
-    fontSize: 11,
-    color: '#545454',
-    marginTop: 4,
-  },
-  iconBottomNav: {
-    width: 26,
-    height: 26,
-    backgroundColor: 'white',
-  },
+  // Style Search Bar
   wrapperBar: {
     paddingTop: 15,
     marginHorizontal: 17,
@@ -111,15 +129,6 @@ const styles = StyleSheet.create({
     top: 7,
     left: 12,
     backgroundColor: 'white',
-  },
-  wrapperIconPromo: {
-    width: 35,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  iconPromo: {
-    width: 34,
-    height: 34,
   },
   wrapperInputSearch: {
     position: 'relative',
@@ -136,27 +145,110 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     marginRight: 15,
   },
-  colorOne: {
+  // Style Promo section
+  // commit revisi styling search and promo
+  wrapperIconPromo: {
+    width: 35,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  iconPromo: {
+    width: 42,
+    height: 34,
+  },
+  // Style Gopay Section
+  // commit adding gopay feature section
+  wrapperFirstSectionGopay: {
+    marginHorizontal: 17,
+    marginTop: 8,
+  },
+  wrapperSectionGopay: {
+    backgroundColor: '#2C5FB8',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 14,
+    borderTopLeftRadius: 4,
+    borderTopRightRadius: 4,
+  },
+  iconGopay: {
+    width: 100,
+    height: 26,
+  },
+  textGopay: {
+    color: 'white',
+    fontSize: 17,
+    fontWeight: 'bold',
+  },
+  wrapperSecondSectionGopay: {
+    backgroundColor: '#2F65BD',
+    paddingTop: 18,
+    paddingBottom: 14,
+    flexDirection: 'row',
+    borderBottomLeftRadius: 4,
+    borderBottomRightRadius: 4,
+  },
+  iconPictureGopay: {
+    width: 36,
+    height: 36,
+  },
+  textSecGopay: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    marginTop: 15,
+    color: 'white',
+  },
+  iconSecGopayOne: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  iconSecGopayTwo: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  iconSecGopayThree: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  iconSecGopayFour: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  // Style Bottom Nav
+  // commit revisi styling bottom nav
+  bottomNav: {
+    height: 54,
+    flexDirection: 'row',
+  },
+  icons: {
+    height: 26,
+    width: 26,
+  },
+  font: {
+    fontSize: 11,
+    color: '#545454',
+    marginTop: 4,
+  },
+  iconOne: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
-  colorTwo: {
+  iconTwo: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
-  colorThree: {
+  iconThree: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
-  colorFour: {
+  iconFour: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
-  colorFive: {
+  iconFive: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
