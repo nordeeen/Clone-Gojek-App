49dd9a0359a503f4610d5868fc493fd9f2921cbd
nordeeen <ahmadnrdnsah@gmail.com>
2022-02-10 14:43:25 +0000

    deleting components in App.js

--- App.js
+++ App.js
@@ -17,15 +17,8 @@ const App = () => {
   );
 };
 
-function HomeScreen() {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
 
-// export default App;
+export default App;
 
 const Stack = createNativeStackNavigator();
 
