516aaf0e9371dc77b3475bf8d2ae080e4ae24ece
nordeeen <ahmadnrdnsah@gmail.com>
2022-02-10 14:08:08 +0000

    moving big parent components

---
+++ src/container/pages/Home/index.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import {View, ScrollView} from 'react-native';
+import GoBanner from '../../../components/molecules/GoBanner';
+import GoInfo from '../../../components/molecules/GoInfo';
+import NewsFeature from '../../../components/molecules/NewsFeature';
+import SearchFeature from '../../../components/molecules/SearchFeature';
+import HomeGopayFeature from '../../../container/organisms/HomeGopayFeature';
+import HomeMainFeature from '../../../container/organisms/HomeMainFeature';
+import NavBar from '../../../container/organisms/NavBar';
+import ScrollableProducts from '../../../container/organisms/ScrollableProducts';
+
+class Home extends Component {
+  state = {};
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <View style={{flex: 1, backgroundColor: 'white'}}>
+            <SearchFeature />
+            <HomeGopayFeature />
+            {/* MAIN FEATURE */}
+            <HomeMainFeature />
+            <View
+              style={{height: 17, backgroundColor: '#F2F2F4', marginTop: 20}}
+            />
+            {/*NEWS SECTION */}
+            <NewsFeature />
+            {/*INTERNAL INFORMATION SECTION*/}
+            <GoInfo />
+            {/*GOFOOD BANNER SECTION*/}
+            <GoBanner />
+            {/*NEARBY GO-FOOD */}
+            <ScrollableProducts />
+          </View>
+        </ScrollView>
+        {/*BOTTOM NAV */}
+        <NavBar />
+      </View>
+    );
+  }
+}
+
+export default Home;
