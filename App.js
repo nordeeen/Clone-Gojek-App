93ee1e16b9f5ded25a1d6b213aec5e68bdb9a6ae
nordeeen <ahmadnrdnsah@gmail.com>
2022-02-10 15:43:15 +0000

    making router navigation component

--- App.js
+++ App.js
@@ -1,26 +1,8 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-
+import Route from './src/config/router';
 
 const App = () => {
-  return (
-  );
+  return <Route />;
 };
-
-
-export default App;
-
-const Stack = createNativeStackNavigator();
-
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={App} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
 
 export default App;
