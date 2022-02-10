7a4dec3908ca7498722422155f82452841bdf59b
nordeeen <ahmadnrdnsah@gmail.com>
2022-02-10 14:44:44 +0000

    remove components import in App.js

--- App.js
+++ App.js
@@ -3,14 +3,6 @@ import {View, ScrollView, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import GoBanner from './src/components/molecules/GoBanner';
-import GoInfo from './src/components/molecules/GoInfo';
-import NewsFeature from './src/components/molecules/NewsFeature';
-import SearchFeature from './src/components/molecules/SearchFeature';
-import HomeGopayFeature from './src/container/organisms/HomeGopayFeature';
-import HomeMainFeature from './src/container/organisms/HomeMainFeature';
-import NavBar from './src/container/organisms/NavBar';
-import ScrollableProducts from './src/container/organisms/ScrollableProducts';
 
 const App = () => {
   return (
