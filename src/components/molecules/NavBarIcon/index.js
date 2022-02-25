2e07f71296c8766aeb1da10a639bc1147ea7b7da
nordeeen <ahmadnrdnsah@gmail.com>
2022-02-25 23:41:08 +0000

    adding touchanbleopacity for button

--- src/components/molecules/NavBarIcon/index.js
+++ src/components/molecules/NavBarIcon/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import {View, Text, Image} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
 const NavBarIcon = props => {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Image source={props.img} style={{height: 26, width: 26}} />
-      <Text
-        style={{
-          fontSize: 11,
-          color: props.active ? '#43AB43' : '#545454',
-          marginTop: 4,
-        }}>
-        {props.title}
-      </Text>
+      <TouchableOpacity>
+        <Image source={props.img} style={{height: 26, width: 26}} />
+        <Text
+          style={{
+            fontSize: 11,
+            color: props.active ? '#43AB43' : '#545454',
+            marginTop: 4,
+          }}>
+          {props.title}
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 };
