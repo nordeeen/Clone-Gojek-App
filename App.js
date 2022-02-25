1a7eb563783947dd5319bf55ac1a03869a1d3df3
nordeeen <ahmadnrdnsah@gmail.com>
2022-02-25 23:51:33 +0000

    fix problem react-native-gesture-handler

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import Route from './src/config/router';
+import {LogBox} from 'react-native';
 
 const App = () => {
   return <Route />;
 };
 
+LogBox.ignoreLogs([
+  "[react-native-gesture-handler] Seems like you're using an old API with gesture components, check out new Gestures system!",
+]);
 export default App;
