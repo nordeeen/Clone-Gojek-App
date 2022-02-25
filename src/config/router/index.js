7b7ae07d6994818f60ab690a28324ba83168d17a
nordeeen <ahmadnrdnsah@gmail.com>
2022-02-25 02:17:07 +0000

    import order and orderdetail

--- src/config/router/index.js
+++ src/config/router/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {Home, NewsDetail} from '../../container/pages';
+import {Home, NewsDetail, OrderDetail, Orders} from '../../container/pages';
 
 const Stack = createNativeStackNavigator();
 
@@ -24,8 +24,8 @@ const OrderStack = () => {
       <Stack.Navigator
         screenOptions={{headerShown: false}}
         initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="NewsDetail" component={NewsDetail} />
+        <Stack.Screen name="Orders" component={Orders} />
+        <Stack.Screen name="OrderDetail" component={OrderDetail} />
       </Stack.Navigator>
     </NavigationContainer>
   );
