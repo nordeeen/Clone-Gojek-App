c9b7f6c411ca97e172566a3bd24cdf42bda43916
nordeeen <ahmadnrdnsah@gmail.com>
2022-02-25 02:14:36 +0000

    create button go detail

--- src/container/pages/Orders/index.js
+++ src/container/pages/Orders/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, View, Button} from 'react-native';
 
 const Orders = () => {
   return (
     <View>
       <Text>Orders Page</Text>;
+      <Button title="Go to Detail" onPress={() => alert('test')} />
     </View>
   );
 };
