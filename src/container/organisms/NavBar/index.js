0fa4f2144c882deccb3304935ef9cc82a7474e63
nordeeen <ahmadnrdnsah@gmail.com>
2022-02-26 04:04:55 +0000

    change function component to class component

--- src/container/organisms/NavBar/index.js
+++ src/container/organisms/NavBar/index.js
@@ -1,38 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {View} from 'react-native';
 import NavBarIcon from '../../../components/molecules/NavBarIcon';
 
-const NavBar = () => {
-  return (
-    <View style={{height: 54, flexDirection: 'row'}}>
-      <NavBarIcon
-        onPress={() => navigation.navigate('Home')}
-        title="Home"
-        img={require('../../../assets/icons/home-active.png')}
-        active
-      />
-      <NavBarIcon
-        onPress={() => navigation.navigate('Orders')}
-        title="Order"
-        img={require('../../../assets/icons/order.png')}
-      />
-      <NavBarIcon
-        onPress={() => navigation.navigate('')}
-        title="Help"
-        img={require('../../../assets/icons/help.png')}
-      />
-      <NavBarIcon
-        onPress={() => navigation.navigate('')}
-        title="Inbox"
-        img={require('../../../assets/icons/inbox.png')}
-      />
-      <NavBarIcon
-        onPress={() => navigation.navigate('')}
-        title="Account"
-        img={require('../../../assets/icons/account.png')}
-      />
-    </View>
-  );
-};
+class NavBar extends Component {
+  render() {
+    return (
+      <View style={{height: 54, flexDirection: 'row'}}>
+        <NavBarIcon
+          onPress={() => navigation.navigate('Home')}
+          title="Home"
+          img={require('../../../assets/icons/home-active.png')}
+          active
+        />
+        <NavBarIcon
+          onPress={() => navigation.navigate('Orders')}
+          title="Order"
+          img={require('../../../assets/icons/order.png')}
+        />
+        <NavBarIcon
+          onPress={() => navigation.navigate('')}
+          title="Help"
+          img={require('../../../assets/icons/help.png')}
+        />
+        <NavBarIcon
+          onPress={() => navigation.navigate('')}
+          title="Inbox"
+          img={require('../../../assets/icons/inbox.png')}
+        />
+        <NavBarIcon
+          onPress={() => navigation.navigate('')}
+          title="Account"
+          img={require('../../../assets/icons/account.png')}
+        />
+      </View>
+    );
+  }
+}
 
 export default NavBar;
