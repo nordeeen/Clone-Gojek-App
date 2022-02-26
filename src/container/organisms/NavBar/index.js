90b11034fe96358c3dd8797053b6362510b11d9e
nordeeen <ahmadnrdnsah@gmail.com>
2022-02-26 00:30:28 +0000

    adding useNavigation

--- src/container/organisms/NavBar/index.js
+++ src/container/organisms/NavBar/index.js
@@ -1,33 +1,36 @@
 import React from 'react';
 import {View} from 'react-native';
 import NavBarIcon from '../../../components/molecules/NavBarIcon';
+import {useNavigation} from '@react-navigation/native';
 
 const NavBar = () => {
+  const navigation = useNavigation();
+
   return (
     <View style={{height: 54, flexDirection: 'row'}}>
       <NavBarIcon
-        onPress={() => this.props.navigation.navigate('Home')}
+        onPress={() => navigation.navigate('Home')}
         title="Home"
         img={require('../../../assets/icons/home-active.png')}
         active
       />
       <NavBarIcon
-        onPress={() => this.props.navigation.navigate('Orders')}
+        onPress={() => navigation.navigate('Orders')}
         title="Order"
         img={require('../../../assets/icons/order.png')}
       />
       <NavBarIcon
-        onPress={() => this.props.navigation.navigate('')}
+        onPress={() => navigation.navigate('')}
         title="Help"
         img={require('../../../assets/icons/help.png')}
       />
       <NavBarIcon
-        onPress={() => this.props.navigation.navigate('')}
+        onPress={() => navigation.navigate('')}
         title="Inbox"
         img={require('../../../assets/icons/inbox.png')}
       />
       <NavBarIcon
-        onPress={() => this.props.navigation.navigate('')}
+        onPress={() => navigation.navigate('')}
         title="Account"
         img={require('../../../assets/icons/account.png')}
       />
