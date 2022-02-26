77399fdf14dd4182f1c510228e4cf6d1ae8742ce
nordeeen <ahmadnrdnsah@gmail.com>
2022-02-26 05:15:52 +0000

    remove onpress button

--- src/container/organisms/NavBar/index.js
+++ src/container/organisms/NavBar/index.js
@@ -1,37 +1,33 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {View} from 'react-native';
 import NavBarIcon from '../../../components/molecules/NavBarIcon';
 
-class NavBar extends Component {
-  render() {
-    return (
-      <View style={{height: 54, flexDirection: 'row'}}>
-        <NavBarIcon
-          onPress={() => this.props.navigation.navigate('Home')}
-          title="Home"
-          img={require('../../../assets/icons/home-active.png')}
-          active
-        />
-        <NavBarIcon
-          onPress={() => navigation.navigate('Orders')}
-          title="Order"
-          img={require('../../../assets/icons/order.png')}
-        />
-        <NavBarIcon
-          title="Help"
-          img={require('../../../assets/icons/help.png')}
-        />
-        <NavBarIcon
-          title="Inbox"
-          img={require('../../../assets/icons/inbox.png')}
-        />
-        <NavBarIcon
-          title="Account"
-          img={require('../../../assets/icons/account.png')}
-        />
-      </View>
-    );
-  }
-}
+const NavBar = () => {
+  return (
+    <View style={{height: 54, flexDirection: 'row'}}>
+      <NavBarIcon
+        title="Home"
+        img={require('../../../assets/icons/home-active.png')}
+        active
+      />
+      <NavBarIcon
+        title="Order"
+        img={require('../../../assets/icons/order.png')}
+      />
+      <NavBarIcon
+        title="Help"
+        img={require('../../../assets/icons/help.png')}
+      />
+      <NavBarIcon
+        title="Inbox"
+        img={require('../../../assets/icons/inbox.png')}
+      />
+      <NavBarIcon
+        title="Account"
+        img={require('../../../assets/icons/account.png')}
+      />
+    </View>
+  );
+};
 
 export default NavBar;
