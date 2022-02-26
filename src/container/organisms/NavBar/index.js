7135020ba5196d0e5e8d555d729b6f50dbb0ae6d
nordeeen <ahmadnrdnsah@gmail.com>
2022-02-26 00:04:35 +0000

    adding onpress in all navbaricon component

--- src/container/organisms/NavBar/index.js
+++ src/container/organisms/NavBar/index.js
@@ -7,23 +7,28 @@ class NavBar extends Component {
     return (
       <View style={{height: 54, flexDirection: 'row'}}>
         <NavBarIcon
+          onPress={() => this.props.navigation.navigate('Home')}
           title="Home"
           img={require('../../../assets/icons/home-active.png')}
           active
         />
         <NavBarIcon
+          onPress={() => this.props.navigation.navigate('Orders')}
           title="Order"
           img={require('../../../assets/icons/order.png')}
         />
         <NavBarIcon
+          onPress={() => this.props.navigation.navigate('')}
           title="Help"
           img={require('../../../assets/icons/help.png')}
         />
         <NavBarIcon
+          onPress={() => this.props.navigation.navigate('')}
           title="Inbox"
           img={require('../../../assets/icons/inbox.png')}
         />
         <NavBarIcon
+          onPress={() => this.props.navigation.navigate('')}
           title="Account"
           img={require('../../../assets/icons/account.png')}
         />
